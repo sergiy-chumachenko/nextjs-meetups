0d7edd22af07fbc893e3be285904f8deed6ae765
Sergiy Chumachenko <chumachenko.sergiy@gmail.com>
2022-11-16 21:44:37 +0200

    adding custom components & css modules

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,14 @@
+import MeetupDetail from "../../components/meetups/MeetupDetail";
+
+function MeetupDetails() {
+    return (
+        <MeetupDetail
+            image="https://upload.wikimedia.org/wikipedia/commons/thumb/a/ae/Risotto_de_gambas%2C_restaurant_Danieli_%28Vienne%2C_Autriche%29.jpg/1024px-Risotto_de_gambas%2C_restaurant_Danieli_%28Vienne%2C_Autriche%29.jpg"
+            title="First Meetup"
+            address='Some Street 5, Some City'
+            description='This is a first meetup'
+        />
+    )
+}
+
+export default MeetupDetails;
