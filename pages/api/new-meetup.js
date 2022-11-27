73120d5a5eb56d765563080b3eba4077ba5ec3f6
Sergiy Chumachenko <chumachenko.sergiy@gmail.com>
2022-11-27 11:01:48 +0200

    connect app to mongo db

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -1,10 +1,19 @@
+import {MongoClient} from "mongodb";
+
 // /api/new-meetup
 
-function handler(req, res) {
+async function handler(req, res) {
     if (req.method === 'POST') {
         const data = req.body;
-        const {title, image, address, description} = data;
-        console.log(data);
+        const client = await MongoClient.connect(
+            "mongodb+srv://mongouser:mongopassword@cluster0.gqwtfbs.mongodb.net/meetups?retryWrites=true&w=majority"
+        )
+        const db = client.db();
+        const meetupsCollection = db.collection('meetups');
+        const result = await meetupsCollection.insertOne({data});
+        console.log(result);
+        await client.close();
+        res.status(201).json({message: "Meetup inserted!"});
     }
 }
 
